576e316148b9b4ea7ddb0d44093fda4a8e9696d2
Giorgio Lovato <giorgio.lovato@ds.mpg.de>
2025-02-11 09:08:58 +0100

    remove axis

--- src/render.jl
+++ src/render.jl
@@ -8,7 +8,7 @@ function Render(p,data)
     anim = @animate for t in ProgressBar(eachindex(data))
         mini = [minimum(last(data)[:,:,j]) for j=1:SO]
         maxi = [maximum(last(data)[:,:,j]) for j=1:SO]
-        hms = [heatmap(data[t][:,:,j],c=:redsblues,clims=(mini[j],maxi[j]),colorbar_title=f"phi_\%d(j)",aspect_ratio=1) for j=1:SO]
+        hms = [heatmap(data[t][:,:,j],c=:redsblues,clims=(mini[j],maxi[j]),colorbar_title=f"phi_\%d(j)",aspect_ratio=1,axis=([],false)) for j=1:SO]
         plot(hms ...,layout=SO)
     end
     mov(anim,"movies/"*savename*".mov",fps=div(60,saveevery),verbose=false,show_msg=false,loop=0)
